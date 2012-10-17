7719eecf2aef3b97359b894689885697154a49f2
Proman21 <snoopyk921@gmail.com>
2012-10-17 15:22:48 +1000

    initial commit
    Signed-off-by: Proman21 <snoopyk921@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7a3644927487cf14dc146f4648ea44b1a094f01bb625065c8579b16ef19ed0efdd2110113e669215988c1213de870a052af7b4fb6c444988d0222ee423af21ce'
